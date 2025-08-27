a04daa494f443c7e32f08e3dac1f56664b699794
mary-silva2007 <claramariassilva07@gmail.com>
2025-08-27 15:49:56 -0300

    27.0.25

---
+++ FrameWorks/React/to-do-list/src/componentes/ToDoForm.js
@@ -0,0 +1,42 @@
+//componente para criar o formulário para inserir uma nova tarefa
+//import do css
+import"./ToDoForm.css";
+
+import { useState } from "react";
+
+const ToDoForm = ({ addTarefa }) => {
+    //estado para armazenar o valor do input (campo para inserir a tarefa)
+  const [tarefa, setTarefa] = useState("");
+  //defenir  o UseState => usa a memoria local do navegador para armazenar as mudanças de estado
+  //[Primeiro campo o armazenamento, segundo campo a função de mudança de estado]
+
+  //função para atualizar o estado co o valor do input
+  const handleSubmit = (e) => {
+    //impedir o comportamento padrão do formulário
+    e.preventDefault();
+    //verificar se o campo não esta vazio
+    if (tarefa.trim() !== "") {
+        //adiciono a tarefa ao array de tarefas
+      addTarefa(tarefa);
+      //Limpo o campo do input
+      setTarefa("");
+    }
+  };
+
+  return (
+    //retorno o formulário (o view)
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={(e) => setTarefa(e.target.value)}
+        value={tarefa}
+        placeholder="Adicionar nova tarefa"
+      />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+};
+
+export default ToDoForm;
+//componente para criar um formulário para inserir uma nova tarefa
+//pode ser realizado em outros componentes (export)
